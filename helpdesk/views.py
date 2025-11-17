618575ed94d3bc64c2d15ecc5e285e3d288187cb
ethalwpl <jakub.kielbik@op.pl>
2025-11-17 17:05:09 +0100

    Zmiana hasla i widok dashboard

--- helpdesk/views.py
+++ helpdesk/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class DashboardView(View):
+    def get(self, request):
+        return render(request, "dashboard.html")
+    
+class ZgloszenieCreateView(View):
+    def get(self, request):
+        return HttpResponse("Tu będzie formularz tworzenia zgłoszenia.")
+
+
+class ZgloszenieDetailView(View):
+    def get(self, request, pk):
+        return HttpResponse(f"Szczegóły zgłoszenia o ID: {pk}")
